aae606f829d1497b072a4a7363021a7621a2f398
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-04-20 15:45:13 -0400

    feat: examples

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, AppRegistry } from 'react-native'
 
-export default class App extends React.Component {
+class FlexboxExamples extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.box}/>
+
+        <View style={styles.box}/>
+
+        {/* Flex sizes an element relative to its siblings */}
+        <View style={[styles.box, {flex: 2}]} />
+
+        {/* Override alignment using alignSelf */}
+        <View style={[styles.box, {alignSelf: 'flex-end'}]}/>
+
+        <View style={styles.box}/>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    // Main axis
+    flexDirection: 'row', // 'column' (default), 'row'
+    justifyContent: 'center', // 'flex-start' (default), 'center', 'flex-end', 'space-between', 'space-around'
+
+    // Cross axis
+    alignItems: 'center' // 'flex-start' (default), 'center', 'flex-end', 'stretch'
   },
-});
+  box: {
+    height: 50, // Overrides alignItems: 'stretch' IF flexDirection: 'row'
+    width: 50, // Overrides alignItems: 'stretch' IF flexDirection: 'column'
+    backgroundColor: '#e76e63',
+    margin: 10,
+  }
+})
+
+export default FlexboxExamples;
